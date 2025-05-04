46c4abbc05f658ba166830ccc555ccbadfa39d04
chaddoy <capdumadag@gmail.com>
2025-05-04 15:17:54 +0800

    fix: outline text styling

--- apps/micro-ui/src/components/Button/Button.tsx
+++ apps/micro-ui/src/components/Button/Button.tsx
@@ -18,10 +18,13 @@ export default function Button({
   return (
     <button
       className={clsx(
-        'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
+        'bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded',
         pill && 'rounded-full',
+        outline && variant === 'primary'
+          ? 'text-blue-700 hover:text-white'
+          : 'text-white',
         outline &&
-          'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded',
+          'bg-transparent hover:bg-blue-500 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded',
         className
       )}
       {...props}
