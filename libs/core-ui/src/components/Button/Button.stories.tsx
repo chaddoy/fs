b73a6b264f8f166e966ac1186e1a0f0aee9f5b92
chaddoy <capdumadag@gmail.com>
2025-06-22 18:26:35 +0800

    refactor: move button stories from apps/ds to libs/core-ui

--- libs/core-ui/src/components/Button/Button.stories.tsx
+++ libs/core-ui/src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { ChevronRight, MailOpen } from 'lucide-react';
-import Button from '@core-ui/components/Button';
+import Button from './Button';
 
 const meta = {
   component: Button,
